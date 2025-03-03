1ef66dc915e6fae092aab5f9a739d50d6998e39f
Albert Thomas <aathomasdev@gmail.com>
2025-03-02 19:43:44 -0500

    Added and Edited Files

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
+
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), Length(min=2, max=50)])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    subject = StringField("Subject", validators=[DataRequired(), Length(max=100)])
+    message = TextAreaField("Message", validators=[DataRequired(), Length(min=10, max=500)])
+    submit = SubmitField("Send Message")
